d8df695dee2fb6c28e491de61f4b69a30bbeb41a
DoudiNCer <doudi@outlook.lv>
2023-09-13 22:13:33 +0800

    OP(L13): time
    Use if instead of HashMap
    Reduce parsing times

--- src/LeetCode/L13/Main.java
+++ src/LeetCode/L13/Main.java
@@ -34,29 +34,29 @@ public class Main {
 
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> mc = new HashMap<>(7);
-        mc.put('I', 1);
-        mc.put('V', 5);
-        mc.put('X', 10);
-        mc.put('L', 50);
-        mc.put('C', 100);
-        mc.put('D', 500);
-        mc.put('M', 1000);
-        int result = 0;
-        char[] chars = s.toCharArray();
-        int i = 0;
-        while (i < chars.length - 1){
-            int pr = mc.get(chars[i]);
-            if (pr < mc.get(chars[i + 1])){
-                pr = mc.get(chars[i + 1]) - pr;
-                i++;
+        int sum = 0;
+        int preNum;
+        preNum = getValue(s.charAt(0));
+        for(int i = 1; i < s.length(); i++){
+            int temp = getValue(s.charAt(i));
+            if(preNum < temp){
+                sum -= preNum;
+            } else{
+                sum += preNum;
             }
-            result += pr;
-            i++;
+            preNum = temp;
         }
-        if (i == chars.length - 1){
-            result += mc.get(chars[i]);
-        }
-        return result;
+        sum += preNum;
+        return sum;
+    }
+    int getValue(char c){
+        if(c == 'I') return 1;
+        if(c == 'V') return 5;
+        if(c == 'X') return 10;
+        if(c == 'L') return 50;
+        if(c == 'C') return 100;
+        if(c == 'D') return 500;
+        if(c == 'M') return 1000;
+        return 0;
     }
 }
