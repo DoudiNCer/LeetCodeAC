e17498ff9d55d24a3d599c61d26bd8aaf74b3f21
DoudiNCer <doudi@outlook.lv>
2023-09-15 13:22:33 +0800

    OP(L20): time
    Determine in advance when parentheses are not paired

--- src/LeetCode/Main.java
+++ src/LeetCode/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println("s.isValid(\"[{}]()\") = " + s.isValid("[{}]()"));
-        System.out.println("s.isValid(\"{()}}{\") = " + s.isValid("{()}}{"));
+        System.out.println("s.isValid(\"{([)}}{\") = " + s.isValid("{()}}{"));
     }
 }
 
