5530aaa9a8d89a7cccf456ccf4afed6ae693710c
DoudiNCer <doudi@outlook.lv>
2023-10-26 21:51:38 +0800

    AC(L58): Length of Last Word

---
+++ src/LeetCode/L58/Main.java
@@ -0,0 +1,25 @@
+package LeetCode.L58;
+
+/**
+ * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+ *
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("s.lengthOfLastWord(\"Hello World\") = " + s.lengthOfLastWord("Hello World"));
+        System.out.println("s.lengthOfLastWord(\"   fly me   to   the moon  \") = " + s.lengthOfLastWord("   fly me   to   the moon  "));
+    }
+}
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = s.length() - s.lastIndexOf(" ") - 1;
+        while (length == 0 && s.length() != 0){
+            s = s.substring(0, s.length() - 1);
+            length = s.length() - s.lastIndexOf(" ") - 1;
+        }
+        return length;
+    }
+}
