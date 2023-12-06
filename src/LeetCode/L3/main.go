232ae2005910d3c2516529129973a77b66073012
DoudiNCer <doudi@outlook.lv>
2023-12-06 12:21:05 +0800

    ACG(L3): Longest Substring Without Repeating Characters

---
+++ src/LeetCode/L3/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	lenS := len(s)
+	if lenS < 2 {
+		return lenS
+	}
+	sm := make(map[byte]int, lenS)
+	maxLen, left, right := 0, 0, 0
+	for right < lenS {
+		x, ok := sm[s[right]]
+		if ok {
+			left = max(left, x+1)
+		}
+		sm[s[right]] = right
+		maxLen = max(maxLen, right-left+1)
+		right++
+	}
+	return maxLen
+}
