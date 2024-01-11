5bbeaded39d94a86f9e0da0a813f20a846f53dc2
DoudiNCer <doudi@outlook.lv>
2024-01-11 21:04:16 +0800

    OP(268): Reduce meaningless calculations

--- src/LeetCode/L268/Main.java
+++ src/LeetCode/L268/Main.java
@@ -48,10 +48,10 @@ public class Main {
 
 class Solution {
     public int missingNumber(int[] nums) {
-        int len = nums.length;
-        int result = len * (len + 1) / 2;
-        for (int num : nums) {
-            result -= num;
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result -= nums[i];
+            result += (i + 1);
         }
         return result;
     }
