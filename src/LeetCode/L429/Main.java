7e97a739b02ff080ad59b356711cd5eb9147eb31
DoudiNCer <doudi@outlook.lv>
2024-02-17 08:47:26 +0800

    AC(429): N-ary Tree Level Order Traversal

---
+++ src/LeetCode/L429/Main.java
@@ -0,0 +1,69 @@
+package LeetCode.L429;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
+ *
+ * 树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。
+ *
+ * 提示：
+ *
+ * - 树的高度不会超过 1000
+ * - 树的节点总数在 [0, 10^4] 之间
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        Node n0 = new Node(1, new LinkedList<>());
+        System.out.println("s.levelOrder(n0) = " + s.levelOrder(n0));
+    }
+}
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new LinkedList<>();
+        if (root == null){
+            return result;
+        }
+        List<Node> preLine = new LinkedList<>();
+        preLine.add(root);
+        while (!preLine.isEmpty()){
+            List<Integer> lineResult = new LinkedList<>();
+            List<Node> nextLine = new LinkedList<>();
+            for (Node node : preLine) {
+                lineResult.add(node.val);
+                if (node.children != null){
+                    for (Node child : node.children) {
+                        if (child != null){
+                            nextLine.add(child);
+                        }
+                    }
+                }
+            }
+            result.add(lineResult);
+            preLine = nextLine;
+        }
+        return result;
+    }
+}
