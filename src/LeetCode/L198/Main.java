c7b2076a1007d71dd79fc7425d1d1de7827e0376
DoudiNCer <doudi@outlook.lv>
2024-03-09 11:20:44 +0800

    OP(198): Reduce unnecessary special situation judgments

--- src/LeetCode/L198/Main.java
+++ src/LeetCode/L198/Main.java
@@ -38,14 +38,9 @@ class Solution {
         if (nums.length == 1){
             return nums[0];
         }
-        if (nums.length == 2){
-            return Math.max(nums[0], nums[1]);
-        }
         int[][] dp = new int[nums.length][2];
         dp[0][1] = nums[0];
-        dp[1][0] = nums[0];
-        dp[1][1] = nums[1];
-        for (int i = 2; i < nums.length; i++){
+        for (int i = 1; i < nums.length; i++){
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
             dp[i][1] = dp[i - 1][0] + nums[i];
         }
