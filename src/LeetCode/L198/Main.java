56f3aab296da96ad4ed5025a53aec87b7d3da756
DoudiNCer <doudi@outlook.lv>
2024-03-09 11:23:09 +0800

    OP(198): One-dimensional dynamic programming compresses space

--- src/LeetCode/L198/Main.java
+++ src/LeetCode/L198/Main.java
@@ -38,12 +38,14 @@ class Solution {
         if (nums.length == 1){
             return nums[0];
         }
-        int[][] dp = new int[nums.length][2];
-        dp[0][1] = nums[0];
+        int[] dp = new int[2];
+        dp[1] = nums[0];
         for (int i = 1; i < nums.length; i++){
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-            dp[i][1] = dp[i - 1][0] + nums[i];
+            int n0 = Math.max(dp[0], dp[1]);
+            int n1 = dp[0] + nums[i];
+            dp[0] = n0;
+            dp[1] = n1;
         }
-        return Math.max(dp[nums.length - 1][0], dp[nums.length - 1][1]);
+        return Math.max(dp[0], dp[1]);
     }
 }
