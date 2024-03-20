c9361e108f0ba1fd42d9bb5467e65b86acc5ec9d
DoudiNCer <doudi@outlook.lv>
2024-03-20 08:21:27 +0800

    AC(1669): Merge In Between Linked Lists

---
+++ src/LeetCode/L1669/ListNode.java
@@ -0,0 +1,36 @@
+package LeetCode.L1669;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode(int[] p){
+        this.val = p[0];
+        int xp = 1;
+        ListNode lp = this;
+        while (xp < p.length){
+            lp.next = new ListNode(p[xp]);
+            lp = lp.next;
+            xp++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+    public String toStrings(){
+        StringBuilder sb = new StringBuilder();
+        ListNode s = this;
+        while (s != null){
+            sb.append(s.val).append(" ");
+            s = s.next;
+        }
+        return sb.toString();
+    }
+}
