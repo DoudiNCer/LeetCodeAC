8888631b4617f39e1075dd7dfa9388c2e5491390
DoudiNCer <doudi@outlook.lv>
2024-03-24 11:58:05 +0800

    AC(322): Coin Change

---
+++ src/LeetCode/L322/Main.java
@@ -0,0 +1,59 @@
+package LeetCode.L322;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ *
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+ *
+ * 你可以认为每种硬币的数量是无限的。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * 示例 2：
+ *
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * 示例 3：
+ *
+ * 输入：coins = [1], amount = 0
+ * 输出：0
+ *
+ *
+ * 提示：
+ *
+ * - 1 <= coins.length <= 12
+ * - 1 <= coins[i] <= 2^31 - 1
+ * - 0 <= amount <= 10^4
+ */
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("s.coinChange(new int[]{186,419,83,408}, 6249) = " + s.coinChange(new int[]{186, 419, 83, 408}, 6249));
+    }
+}
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        Arrays.sort(coins);
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = coins[0]; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                } else {
+                    break;
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
