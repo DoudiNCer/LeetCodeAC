a6d7a5d78c1b4555299fd0e7f9abb8692c5f2309
DoudiNCer <doudi@outlook.lv>
2024-12-22 15:17:28 +0800

    OPG64: Two-dimensional dynamic programming degenerates into one-dimensional dynamic programming, which occupies less space

--- src/LeetCode/L64/main.go
+++ src/LeetCode/L64/main.go
@@ -32,22 +32,21 @@ func main() {
 
 func minPathSum(grid [][]int) int {
 	m, n := len(grid), len(grid[0])
-	dp := make([][]int, m)
+	dp := make([][]int, 2)
 	dp[0] = make([]int, n)
 	dp[0][0] = grid[0][0]
 	for j := 1; j < n; j++ {
 		dp[0][j] = dp[0][j-1] + grid[0][j]
 	}
 	for i := 1; i < m; i++ {
-		dp[i] = make([]int, n)
-		dp[i][0] = dp[i-1][0] + grid[i][0]
-	}
-	for i := 1; i < m; i++ {
+		dp[1] = make([]int, n)
+		dp[1][0] = grid[i][0] + dp[0][0]
 		for j := 1; j < n; j++ {
-			dp[i][j] = minInt(dp[i][j-1], dp[i-1][j]) + grid[i][j]
+			dp[1][j] = minInt(dp[1][j-1], dp[0][j]) + grid[i][j]
 		}
+		dp[0] = dp[1]
 	}
-	return dp[m-1][n-1]
+	return dp[0][n-1]
 }
 
 func minInt(a, b int) int {
