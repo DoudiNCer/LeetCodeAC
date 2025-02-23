1b3df1c72b641a9b24bf2eb10ff64cf6a02af20f
DoudiNCer <doudi@outlook.lv>
2025-02-23 15:34:46 +0800

    OPG2209: 2D dynamic programming is simplified to rolling updates

--- src/LeetCode/L2209/main.go
+++ src/LeetCode/L2209/main.go
@@ -40,26 +40,20 @@ func main() {
 
 func minimumWhiteTiles(floor string, numCarpets int, carpetLen int) int {
 	n := len(floor)
-	dp := make([][]int, n+1)
-	{
-		cnt := 0
-		for i := range dp {
-			dp[i] = make([]int, numCarpets+1)
-			for j := 0; j <= numCarpets; j++ {
-				dp[i][j] = cnt
-			}
-			if i < n {
-				cnt += int(floor[i] - '0')
-			}
-		}
-	}
+	dp := make([][]int, 2)
+	dp[0] = make([]int, n+1)
+	dp[1] = make([]int, n+1)
 	for i := 1; i <= n; i++ {
-		for j := 1; j <= numCarpets; j++ {
-			dp[i][j] = dp[i-1][j] + int(floor[i-1]-'0')
-			dp[i][j] = min(dp[i][j], dp[nop(i-carpetLen)][j-1])
-		}
+		dp[0][i] = dp[0][i-1] + int(floor[i-1]-'0')
 	}
-	return dp[n][numCarpets]
+	for j := 1; j <= numCarpets; j++ {
+		for i := 1; i <= n; i++ {
+			dp[1][i] = dp[1][i-1] + int(floor[i-1]-'0')
+			dp[1][i] = min(dp[1][i], dp[0][nop(i-carpetLen)])
+		}
+		dp[0], dp[1] = dp[1], dp[0]
+	}
+	return dp[0][n]
 }
 
 func min(a, b int) int {
