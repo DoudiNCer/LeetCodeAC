8a0fcd6f59605631c0e468bf1d572fc3ca1b469d
DoudiNCer <doudi@outlook.lv>
2025-03-01 14:00:47 +0800

    OPG2460: Reduce the lifetime of variable i when moving 0

--- src/LeetCode/L2460/main.go
+++ src/LeetCode/L2460/main.go
@@ -51,22 +51,21 @@ func applyOperations(nums []int) []int {
 	n := len(nums)
 	for i := 0; i < n-1; i++ {
 		if nums[i] == nums[i+1] {
-			nums[i], nums[i+1] = nums[i]*2, 0
+			nums[i] <<= 1
+			nums[i+1] = 0
 		}
 	}
-	l, r := 0, 0
-	for r < n {
-		if nums[r] == 0 {
-			r++
+	x := 0
+	for i := 0; i < n; i++ {
+		if nums[i] == 0 {
 			continue
 		}
-		nums[l] = nums[r]
-		l++
-		r++
+		nums[x] = nums[i]
+		x++
 	}
-	for l < n {
-		nums[l] = 0
-		l++
+	for x < n {
+		nums[x] = 0
+		x++
 	}
 	return nums
 }
