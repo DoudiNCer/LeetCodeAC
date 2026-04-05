bd8e5a478f64909dec1e0b8939aa8d9fcb226e01
DoudiNCer <doudi@outlook.lv>
2026-04-05 19:51:36 +0800

    ACR(L657): Robot Return to Origin

---
+++ src/LeetCode/L657/solution.rs
@@ -0,0 +1,34 @@
+use super::Solution;
+
+/*
+  在二维平面上，有一个机器人从原点 (0, 0) 开始。给出它的移动顺序，判断这个机器人在完成移动后是否在 (0, 0) 处结束。
+
+  移动顺序由字符串 moves 表示。字符 move[i] 表示其第 i 次移动。机器人的有效动作有 R（右），L（左），U（上）和 D（下）。
+
+  如果机器人在完成所有动作后返回原点，则返回 true。否则，返回 false。
+
+  注意：机器人“面朝”的方向无关紧要。 “R” 将始终使机器人向右移动一次，“L” 将始终向左移动等。此外，假设每次移动机器人的移动幅度相同。
+
+  提示:
+
+  1 <= moves.length <= 2 * 104
+  moves 只包含字符 'U', 'D', 'L' 和 'R'
+*/
+
+impl Solution {
+    pub fn judge_circle(moves: String) -> bool {
+        let mut loc = (0, 0);
+
+        for c in moves.chars() {
+            match c {
+                'L' => { loc.0 = loc.0 - 1 }
+                'R' => { loc.0 = loc.0 + 1 }
+                'U' => { loc.1 = loc.1 + 1 }
+                'D' => { loc.1 = loc.1 - 1 }
+                _ => {}
+            }
+        }
+
+        loc.0 == 0 && loc.1 == 0
+    }
+}
--- src/LeetCode/mod.rs
+++ src/LeetCode/mod.rs
@@ -1,2 +1,3 @@
 pub mod L1;
+pub mod L657;
 pub mod L2839;
