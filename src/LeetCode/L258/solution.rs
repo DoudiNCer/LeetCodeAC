198c79282c7719ad8af02ef0b418769b62fcee3d
DoudiNCer <doudi@outlook.lv>
2026-04-06 13:42:19 +0800

    ACR(L258): Add Digits

---
+++ src/LeetCode/L258/solution.rs
@@ -0,0 +1,25 @@
+use super::Solution;
+
+
+/*
+    给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。返回这个结果。
+*/
+
+impl Solution {
+    pub fn add_digits(num: i32) -> i32 {
+        if num > 9 {
+            let mut nn = 0;
+            let mut mnum = num;
+            loop {
+                if mnum == 0 {
+                    break;
+                }
+                nn += mnum % 10;
+                mnum /= 10;
+
+            }
+            return Solution::add_digits(nn)
+        }
+        num
+    }
+}
