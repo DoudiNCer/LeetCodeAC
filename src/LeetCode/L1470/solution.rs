a7f54b2d34cf19025a5fb492e9cd702cffe1fcfd
DoudiNCer <doudi@outlook.lv>
2026-04-09 00:03:59 +0800

    OPR(L1470): Init result Vec with capacity.

--- src/LeetCode/L1470/solution.rs
+++ src/LeetCode/L1470/solution.rs
@@ -15,7 +15,7 @@ use std::net::UdpSocket;
 
 impl Solution {
     pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
-        let mut result: Vec<i32> = Vec::new();
+        let mut result: Vec<i32> = Vec::with_capacity((n << 1) as usize);
         for i in 0..n {
             result.push(nums[i as usize]);
             result.push(nums[(i + n) as usize])
