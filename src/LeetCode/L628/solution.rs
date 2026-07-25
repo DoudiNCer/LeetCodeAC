5fed5cca7b8d8618e3f5e26fe4eac30b8ccfd65f
DoudiNCer <doudi@outlook.lv>
2026-07-26 01:19:52 +0800

    OPR(L628): Simplify by code similarity

--- src/LeetCode/L628/solution.rs
+++ src/LeetCode/L628/solution.rs
@@ -11,22 +11,9 @@ use super::Solution;
 
 impl Solution {
     pub fn maximum_product(nums: Vec<i32>) -> i32 {
-        let (mut pos, mut neg) = (0, 0);
-        for num in nums.iter() {
-            if *num > 0 {
-                pos += 1;
-            } else if *num < 0 {
-                neg += 1;
-            }
-        }
         let mut nums = nums;
         nums.sort();
         let n = nums.len();
-        if pos == 0 {
-            return nums[n - 1] * nums[n - 2] * nums[n - 3];
-        }
-        let mut res = nums[n - 1] * nums[n - 2] * nums[n - 3];
-        res = res.max(nums[0] * nums[1] * nums[n - 1]);
-        res
+        (nums[n - 1] * nums[n - 2] * nums[n - 3]).max(nums[0] * nums[1] * nums[n - 1])
     }
 }
